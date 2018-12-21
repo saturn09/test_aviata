44e590a9b5323c75ca5f150d10b8125b6d3606df
nabdreimov <nabdreimov@wooppay.com>
2018-12-21 14:35:24 +0600

    Iterate through routes and dates

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+from itertools import product
+from dateutil.rrule import rrule, DAILY
 from pprint import pprint
 
-from core.main import Api, Client
+from core.main import Api
 from core.creds import Route
 
 if __name__ == '__main__':
     api = Api()
-    c = Client(api)
-    flights = c.list_flights(Route.ALA_CIT, datetime(2019, 1, 1))
-    pprint(flights)
+    for route, date in product(
+            [r for r in Route],
+            [d for d in rrule(freq=DAILY, dtstart=datetime.now(), count=30)]
+    ):
+        flights = api.list_flights(Route.ALA_CIT, datetime(2019, 1, 1))
+        pprint(flights)
