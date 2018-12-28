88c3e813b35306b07b3179ad60a3bd89f3165d9f
nabdreimov <nabdreimov@wooppay.com>
2018-12-28 18:11:46 +0600

    Send requests partially by limiting request rate per sec

--- entrypoint.py
+++ entrypoint.py
@@ -2,33 +2,41 @@ import asyncio
 from datetime import datetime
 from itertools import product
 from dateutil.rrule import rrule, DAILY
-from pprint import pprint
 
-from core.entities import Api
+from core.entities import Api, View
 from core.creds import Route
 
-import pandas as pd
+REQUEST_PER_SEC = 65
 
 
 async def main():
     api = Api()
     tasks = []
 
-    route_date = product(
+    route_date = list(product(
         [r for r in Route],
-        [d for d in rrule(freq=DAILY, dtstart=datetime.now(), count=30)])
+        [d for d in rrule(freq=DAILY, dtstart=datetime.now(), count=31)]))
 
-    for route, date in route_date:
-        task = asyncio.ensure_future(api.list_flights(route, date))
-        tasks.append(task)
+    flights = []
 
-    flights = await asyncio.gather(*tasks)
-    cheapest = [min(f, key=lambda x: x.price) for f in flights if f]
-    cheapest_as_dict = [c.to_dict() for c in cheapest]
+    while route_date:
+        """
+        Отправка запросов частями, потому что сервер возвращает статусы 500 и 502 при
+        большом кол-ве запросов в секунду.
+        """
+        for route, date in route_date[:REQUEST_PER_SEC]:
+            task = asyncio.ensure_future(api.list_flights(route, date))
+            tasks.append(task)
 
-    table = pd.DataFrame(cheapest_as_dict)
-    table.to_csv('test.csv', sep=',', encoding='utf-8')
+        flights.extend(await asyncio.gather(*tasks))
+        del tasks[:REQUEST_PER_SEC]
+        del route_date[:REQUEST_PER_SEC]
 
+    cheapest = [min(route_flights, key=lambda flight: flight.price).to_dict()
+                for route_flights in flights if route_flights]
+
+    View.to_csv(cheapest,
+                f'flights_for_30_days_from_{datetime.today().strftime("%Y-%m-%d")}')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
